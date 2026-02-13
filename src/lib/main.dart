98783a7995c3d8446519dd28e4055503638cbcda
seanr89 <srafferty89@gmail.com>
2026-02-13 21:20:50 +0000

    feat: Integrate `flutter_dotenv` for environment variable management and remove the dedicated web build workflow.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -12,9 +12,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'auth_service.dart';
 import 'firebase_options.dart';
 import 'login_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     StreamProvider<User?>.value(
